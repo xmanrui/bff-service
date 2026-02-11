0219cfe0afb75f3657cf7521f456b8f23ed3fc00
xmanrui <841206367@qq.com>
2026-02-12 02:49:28 +0800

    Hello world

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ WELCOME_HTML = """<!DOCTYPE html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>BFF Service</title>
+<title>Hello world！</title>
 <style>
   * { margin: 0; padding: 0; box-sizing: border-box; }
   body {
